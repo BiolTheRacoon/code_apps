72a69c642ddc7b5c6e66620794e3bcf3032f0d04
BiolTheRacoon <madv1710@gmail.com>
2025-08-01 13:36:38 +0200

    Added number guess game to repository

---
+++ Number_guess.py
@@ -0,0 +1,73 @@
+import random as rnd
+import os
+import time
+
+def number_guess():
+    number = rnd.randint(1, 100)
+    count = 0
+
+    while True:
+        guess = input("Guess a number between 1 and 100: ")
+        guess = int(guess)
+
+        if guess > number:
+            print("Guess lower!")
+            count += 1
+
+        elif guess < number:
+            print("Guess higher!")
+            count += 1
+
+        else:
+            print("Correct!")
+            time.sleep(2)
+            os.system('clear')
+            break
+
+
+    print(f"You guessed the number in {count} tries!")
+    time.sleep(2)
+    os.system('clear')
+
+number_guess()
+
+print("wish to play again?")
+play_again = input("[y/n]: ")
+
+if play_again == "y":
+    number_guess()
+elif play_again == "n":
+    print("Thanks for playing!")
+    os.system('clear')
+    print("Exiting the game  [   ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [.  ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [.. ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [...]")
+    time.sleep(1)
+    os.system('clear')
+    os._exit(0)
+else:
+    print("Invalid input.")
+    time.sleep(2)
+    os.system('clear')
+    print("Exiting the game  [   ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [.  ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [.. ]")
+    time.sleep(1)
+    os.system('clear')
+    print("Exiting the game  [...]")
+    time.sleep(1)
+    os.system('clear')
+    os._exit(0) 
+ 
+
